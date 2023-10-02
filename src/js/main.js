351f5c9c663e884c9a9306e0015c41bc88d13b1b
Mayte <mayte710@hotmail.com>
2023-10-02 10:29:27 +0100

    finishing details

--- src/js/main.js
+++ src/js/main.js
@@ -183,5 +183,3 @@ function addEventBtnDelete() {
 btnSearch.addEventListener('click', handleClickSearch);
 renderPreShows();
 btnclearFavs.addEventListener('click', handleClickClear);
-
-// Me falta que se borre solo al darle a la X, y preguntar como meter

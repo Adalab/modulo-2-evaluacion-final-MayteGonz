19b3d63e5e99a5e1c84c2b589324fe4786ddcb21
Mayte <mayte710@hotmail.com>
2023-10-02 09:38:03 +0100

    add pointer to cards

--- src/js/main.js
+++ src/js/main.js
@@ -82,6 +82,9 @@ function renderOneShow(oneShow, isfavorite) {
     cardShow.classList.add('favorite');
     mark.classList.remove('fa-regular');
     mark.classList.add('fa-solid');
+    if (!isfavorite) {
+      cardShow.classList.add('clickable');
+    }
   }
   topCard.appendChild(mark);
 
